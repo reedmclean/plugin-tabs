4eb2f183b91938b2c3711bf1f3271619a5472cfc
Ozzy Osborne <bardweller@gmail.com>
2017-02-10 11:15:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -23,30 +23,12 @@ function createTabBody(block, i, isActive, book) {
         return new Promise((resolve,reject) => {
                 book.renderBlock( 'asciidoc' , block.body )
                     .then(function(rendered){ 
-                         console.log("Tab "+i+" "+block.kwargs.name+" raw:"+block.body+" rendered:"+rendered);
                          resolve( '<div class="tab' + (isActive? ' active' : '') + '" data-tab="' + i + '">' + rendered + '</div>' );
                 });
         });
     }else if(block.kwargs.type == "markdown"){
         return new Promise((resolve,reject) => {
                 book.renderBlock( 'markdown' , block.body )
-                    .then(function(rendered){ 
-                         console.log("Tab "+i+" "+block.kwargs.name+" raw:"+block.body+" rendered:"+rendered);
-                         resolve( '<div class="tab' + (isActive? ' active' : '') + '" data-tab="' + i + '">' + rendered + '</div>' );
-                });
-        });        
-    }else if(block.kwargs.type == "i-text" || block.kwargs.type == "i-asciidoc"){        
-        return new Promise((resolve,reject) => {
-                book.renderInline( 'asciidoc' , block.body )
-                    .then(function(rendered){ 
-                         console.log("Tab "+i+" "+block.kwargs.name+" raw:"+block.body+" rendered:"+rendered);
-                         var wrapped =  '<div class="tab' + (isActive? ' active' : '') + '" data-tab="' + i + '">' + rendered + '</div>';
-                         resolve( wrapped );
-                });
-        });
-    }else if(block.kwargs.type == "i-markdown"){
-        return new Promise((resolve,reject) => {
-                book.renderInline( 'markdown' , block.body )
                     .then(function(rendered){ 
                          resolve( '<div class="tab' + (isActive? ' active' : '') + '" data-tab="' + i + '">' + rendered + '</div>' );
                 });
